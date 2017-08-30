0c0c4af14024298d672625375ed95bf29fd16013
Yan Zhulanow <Yan.Zhulanow@jetbrains.com>
2017-08-30 23:02:41 +0300

    ~

--- libraries/tools/kotlin-gradle-plugin/src/agp25/kotlin/org/jetbrains/kotlin/gradle/plugin/android/KotlinLintTask.kt
+++ libraries/tools/kotlin-gradle-plugin/src/agp25/kotlin/org/jetbrains/kotlin/gradle/plugin/android/KotlinLintTask.kt
@@ -1,22 +1,12 @@
 package org.jetbrains.kotlin.gradle.plugin.android
 
-import com.android.build.FilterData
-import com.android.build.OutputFile
 import com.android.build.gradle.internal.dsl.LintOptions
-import com.android.build.gradle.internal.ide.DependenciesImpl
-import com.android.build.gradle.internal.ide.FilterDataImpl
-import com.android.build.gradle.internal.ide.JavaArtifactImpl
-import com.android.build.gradle.internal.ide.JavaLibraryImpl
-import com.android.build.gradle.internal.ide.level2.AndroidLibraryImpl
 import com.android.builder.model.LintOptions as ILintOptions
 import com.android.build.gradle.internal.scope.VariantScope
 import com.android.build.gradle.internal.tasks.BaseTask
 import com.android.build.gradle.tasks.Lint
 import com.android.builder.core.AndroidBuilder
-import com.android.builder.dependency.MavenCoordinatesImpl
-import com.android.builder.internal.ClassFieldImpl
 import com.android.builder.model.*
-import com.android.builder.model.level2.DependencyGraphs
 import com.android.ide.common.repository.GradleCoordinate
 import com.android.manifmerger.ManifestMerger2
 import com.android.repository.Revision
@@ -26,107 +16,40 @@ import com.android.tools.lint.Reporter
 import com.android.tools.lint.checks.ApiParser
 import com.intellij.openapi.project.Project as IdeaProject
 import com.android.tools.lint.client.api.LintRequest
+import com.esotericsoftware.kryo.Kryo
+import com.esotericsoftware.kryo.io.Input
+import com.esotericsoftware.kryo.io.Output
+import com.esotericsoftware.kryo.serializers.JavaSerializer
+import com.esotericsoftware.reflectasm.ConstructorAccess
 import com.google.common.collect.Sets
 import com.google.gson.*
-import com.google.gson.stream.JsonReader
-import com.google.gson.stream.JsonWriter
 import com.intellij.util.PathUtil
+import de.javakaffee.kryoserializers.guava.ImmutableListSerializer
 import org.gradle.api.Project
 import org.gradle.api.file.FileCollection
 import org.gradle.api.internal.AbstractTask
 import org.gradle.api.tasks.ParallelizableTask
 import org.gradle.api.tasks.TaskAction
-import org.gradle.internal.impldep.com.fasterxml.jackson.databind.annotation.JsonSerialize
 import org.gradle.tooling.provider.model.ToolingModelBuilderRegistry
 import org.jetbrains.kotlin.gradle.plugin.Android25ProjectHandler.Companion.LINT_WITH_KOTLIN_CONFIGURATION_NAME
-import org.jetbrains.kotlin.gradle.plugin.android.mappings.KoAndroidArtifact
-import org.jetbrains.kotlin.gradle.plugin.android.mappings.KoAndroidArtifactOutput
-import org.jetbrains.kotlin.gradle.plugin.android.mappings.KoOutputFile
-import org.jetbrains.kotlin.gradle.plugin.android.mappings.KoVariant
 import org.jetbrains.uast.UastContext
 import org.objectweb.asm.ClassVisitor
 import org.objectweb.asm.tree.AbstractInsnNode
 import org.objectweb.asm.tree.analysis.AnalyzerException
+import org.objenesis.strategy.BaseInstantiatorStrategy
+import org.objenesis.strategy.StdInstantiatorStrategy
 import org.xmlpull.v1.XmlPullParserException
+import java.io.ByteArrayInputStream
+import java.io.ByteArrayOutputStream
 import java.io.File
 import java.lang.reflect.Field
 import java.net.URL
 import java.net.URLClassLoader
+import java.util.*
 import lombok.ast.Node as LombokNode
 
 @ParallelizableTask
 open class KotlinLintTask : AbstractTask() {
-    private companion object {
-        private val CLASS_MAPPINGS = listOf(
-                Variant::class.java to KoVariant::class.java,
-                ILintOptions::class.java to LintOptions::class.java,
-//                AndroidArtifact::class.java to KoAndroidArtifact::class.java,
-                AndroidArtifactOutput::class.java to KoAndroidArtifactOutput::class.java,
-                ClassField::class.java to ClassFieldImpl::class.java,
-                JavaArtifact::class.java to JavaArtifactImpl::class.java,
-                FilterData::class.java to FilterDataImpl::class.java,
-                OutputFile::class.java to KoOutputFile::class.java,
-                Dependencies::class.java to DependenciesImpl::class.java,
-                AndroidLibrary::class.java to AndroidLibraryImpl::class.java,
-                JavaLibrary::class.java to JavaLibraryImpl::class.java,
-                MavenCoordinates::class.java to MavenCoordinatesImpl::class.java
-        ).map { it.first.name to it.second.name }
-
-        fun createGson(newClassLoader: ClassLoader): Gson {
-            val exclusionRules: ExclusionStrategy = object : ExclusionStrategy {
-                val excludedClassNames = listOf(
-                        SigningConfig::class.java,
-                        AaptOptions::class.java,
-                        ArtifactMetaData::class.java,
-                        JavaCompileOptions::class.java,
-                        ProductFlavorContainer::class.java,
-                        SyncIssue::class.java,
-                        BuildTypeContainer::class.java,
-                        NativeToolchain::class.java,
-                        AndroidArtifactOutput::class.java,
-                        NativeLibrary::class.java,
-                        ProductFlavor::class.java,
-                        InstantRun::class.java,
-                        DependencyGraphs::class.java
-                ).map { it.name }
-
-                val excludedFieldNames = listOf("__dyn_obj__", "__meta_class__")
-
-                override fun shouldSkipClass(clazz: Class<*>) = clazz.name in excludedClassNames
-                override fun shouldSkipField(f: FieldAttributes) = f.name in excludedFieldNames
-            }
-
-            val androidArtifact = Class.forName(AndroidArtifact::class.java.name, false, newClassLoader)
-
-            return GsonBuilder()
-                    .addSerializationExclusionStrategy(exclusionRules)
-                    .addDeserializationExclusionStrategy(exclusionRules)
-                    .registerTypeAdapter(androidArtifact,
-                            JsonSerializer<AndroidArtifact> { src, _, context ->
-                        context.serialize(src, src.javaClass)
-                        context.serialize(src.outputs)
-                    })
-//                    .registerTypeAdapter(androidArtifact,
-//                            JsonDeserializer<Any> { json, typeOfT, context ->
-//                        val koAndroidArtifact = Class.forName(KoAndroidArtifact::class.java.name, true, newClassLoader)
-//                        val artifact = context.deserialize<Any>(json, koAndroidArtifact)
-//                        koAndroidArtifact.getDeclaredField("outputs").set(context.deserialize)
-//                        return@JsonDeserializer artifact
-//                    }
-                    .apply {
-                        for ((originalName, mappedName) in CLASS_MAPPINGS) {
-                            val originalClass = Class.forName(originalName, true, newClassLoader)
-                            val mappedClass = Class.forName(mappedName, true, newClassLoader)
-
-                            registerTypeAdapter(originalClass, JsonDeserializer<Any> { json, type, context ->
-                                context.deserialize(json, mappedClass)
-                            })
-                        }
-                    }
-                    .create()
-        }
-    }
-
     lateinit var javaLintTask: Lint
 
     @TaskAction
@@ -148,12 +71,13 @@ open class KotlinLintTask : AbstractTask() {
 
         val topmostClassLoader = findClassLoaderForLint(javaClass.classLoader)
         val classLoaderForLint = URLClassLoader(collectLibrariesForClasspath(project), topmostClassLoader)
-        val gson = createGson(classLoaderForLint)
 
-        val buildTools = mapNotNull(gson, _buildTools, classLoaderForLint)
-        val lintOptions = map(gson, _lintOptions, classLoaderForLint)
-        val androidProject = mapNotNull(gson, createAndroidProject(project, _toolingRegistry), classLoaderForLint)
-        val variantScope = map(gson, _variantScope, classLoaderForLint)
+        val mapper = LintObjectsMapper(classLoaderForLint)
+
+        val buildTools = mapper.mapNotNull(_buildTools)
+        val lintOptions = mapper.map(_lintOptions)
+        val androidProject = mapper.mapNotNull(createAndroidProject(project, _toolingRegistry))
+        val variantScope = mapper.map(_variantScope)
 
         val executorClass = Class.forName(KotlinLintExecutor::class.java.name, true, classLoaderForLint)
         val executor = executorClass.constructors.single().newInstance(
@@ -161,25 +85,6 @@ open class KotlinLintTask : AbstractTask() {
                 reportsDir, manifestReportFile, outputsDir, manifestsForVariant, variantScope)
 
         executorClass.getDeclaredMethod("lint").invoke(executor)
-    }
-
-    private fun map(gson: Gson, obj: Any?, newClassLoader: ClassLoader): Any? {
-        return if (obj == null) null else mapNotNull(gson, obj, newClassLoader)
-    }
-
-    private fun mapNotNull(gson: Gson, obj: Any, newClassLoader: ClassLoader): Any {
-        fun findTargetClass(clazz: Class<*>) =
-                if (clazz.name.endsWith("_Decorated")) clazz.superclass ?: clazz else clazz
-
-        val className = findTargetClass(obj.javaClass).let { it.canonicalName ?: it.name }
-        val clazzFromNewClassLoader = Class.forName(className, false, newClassLoader)
-
-        try {
-            val data = gson.toJson(obj)
-            return gson.fromJson(data, clazzFromNewClassLoader)
-        } catch (e: Throwable) {
-            throw RuntimeException("Can't map $className", e)
-        }
     }
 
     private fun findClassLoaderForLint(classLoader: ClassLoader): ClassLoader {
@@ -241,6 +146,12 @@ open class KotlinLintTask : AbstractTask() {
         val gson = findLibraryFor<Gson>()
         val kxml = findLibraryFor<XmlPullParserException>()
 
+        val kryo = findLibraryFor<Kryo>()
+        val objenesis = findLibraryFor<BaseInstantiatorStrategy>()
+        val kryoSerializers = findLibraryFor<ImmutableListSerializer>()
+        val reflectAsm = findLibraryFor<ConstructorAccess<*>>()
+        val minlog = findLibraryFor<com.esotericsoftware.minlog.Log>()
+
         // Lint
         val lint = findLibraryFor<Reporter>()
         val lintApi = findLibraryFor<LintRequest>()
@@ -252,6 +163,7 @@ open class KotlinLintTask : AbstractTask() {
                 layoutLibApi, lombokAst,
                 asm, asmTree, asmAnalysis,
                 gson, kxml,
+                kryo, objenesis, kryoSerializers, reflectAsm, minlog,
                 lint, lintApi, lintChecks).apply {
             project.logger.warn("Lint with Kotlin classpath: ${this.joinToString { it.path }}")
         }
